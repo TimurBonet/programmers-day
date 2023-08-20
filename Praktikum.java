42d2fd4538c2c0e844af10b576d919e30f413023
TimurBonet <142611242+TimurBonet@users.noreply.github.com>
2023-08-20 18:33:23 +0700

    Add files via upload
    Изменения: проведён рефакторинг метода isLeapYear(), скорректирован вывод дня программиста високосный 13.09 / обычный 12.09

--- Praktikum.java
+++ Praktikum.java
@@ -10,20 +10,20 @@ public class Praktikum {
 
         if (isLeapYear(year)) {
             // здесь нужно вывести результат
-            System.out.println("12.09." + year);
+            System.out.println("13.09." + year);
         }
-        else System.out.println("13.09." + year);
+        else System.out.println("12.09." + year);
     }
 
     public static boolean isLeapYear(int year) {
         // здесь нужно определить, является ли переданный год високосным
-        boolean isTrue = false;
-        if(year%4==0 && year%100!=0 ){
-            isTrue = true;
+
+        if(year % 4 == 0 && year % 100 != 0 ){
+            return true;
         }
-            else if (year%400==0){
-                isTrue = true;
+            else if (year % 400 == 0){
+                return true;
             }
-            return isTrue;
+            return false;
     }
 }
